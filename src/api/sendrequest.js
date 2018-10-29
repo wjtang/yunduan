97a05176dca0a62c840cffeb61305422c576ca32
WJT\gesangdianzi <734688296@qq.com>
2018-10-29 09:26:40 +0800

    18.10.29 frit commit

---
+++ src/api/sendrequest.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+	const base_url = '', //接口地址
+	postreq(url,data){
+		return new Promise((resolve, reject) => {
+			axios.post(base_url+url,qs.stringify(data)).then(response => {
+				// console.log(response.data);
+				resolve(response.data)
+			})
+		})
+	},
+	getreq(url,data){
+		return new Promise((resolve, reject) => {
+			axios.get()
+		})
+	}
+}
